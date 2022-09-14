4a6d7f0a7cbcf19a9b3aeee53e9af85c8efc0070
mikihiro makino <16id014mmiki@gmail.com>
2022-09-14 10:39:22 +0900

    initial commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,9 @@
+class Prototype < ApplicationRecord
+    validates :image, presence: true
+    validates :title, presence: true
+    validates :cath_copy, presence: true
+    validates :concept, presence: true
+    has_many :comments, dependent: :destroy
+    belongs_to :user
+    has_one_attached :image
+end
